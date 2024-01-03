997a0fe5c2615dfa30a5b05fd7ad226e77a59225
Matthew Flynn <90197912+mpflynnx@users.noreply.github.com>
2024-01-03 10:44:00 +0000

    #34 Pass by reference usage (#35)

--- d.c
+++ d.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 
-void add_one(int array[], int length);
+void add_one(int *a, int *b, int *c);
 
 int main(void)
 {
 
-    int a = 5;
-    int *p;
-    p = &a;
-    printf("p: %p\n", p);
-    printf("&a: %p\n", &a);
-
-    int b = 10;
-    printf("&b: %p\n", &b);
-
-    a = a + *p;
-    printf("%d\n", *p);
-
-    p = &b;
-    printf("p: %p\n", p);
-
-    *p = *p + 1; // b = b + 1
-
-    printf("b: %d\n", b);
+    int x1 = 1, x2 = 2, x3 = 3;
+    add_one(&x1, &x2, &x3);
+    printf("x1: %d\n", x1);
+    printf("x2: %d\n", x2);
+    printf("x3: %d\n", x3);
 
     return 0;
+}
+
+void add_one(int *a, int *b, int *c)
+{
+    *a = *a + 1;
+    *b = *b + 1;
+    *c = *c + 1;
 }
