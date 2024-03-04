5963bc5591b952a92d243bdacbe613e8fe4edf26
mpflynnx <mpflynnx@gmail.com>
2024-03-04 14:50:07 +0000

    Pointers #31 Update docs null pointers

--- d.c
+++ d.c
@@ -1,32 +1,11 @@
 #include <stdio.h>
 
-struct Foo1
-{
-    short a; // will have 2 bytes of padding after a
-    int b;
-    short c; // will have 2 bytes of padding after c
-};
-
-struct Foo2
-{
-    int b;
-    short a;
-    short c;
-};
-
 int main()
 {
-    struct Foo1 foo1 = {0,0,0}; // prints 12
-    struct Foo2 foo2 = {0,0,0}; // prints 8
 
-    printf("The size of Foo1 is %ld bytes\n", sizeof(foo1));
-    printf("The size of Foo2 is %ld bytes\n", sizeof(foo2));
+    int* p = NULL;
+    int a = 5;
+    p = &a; // Assign memory address of a to p
 
     return 0;
-}
-
-void add_one(int array[], int length)
-{
-    printf("Array passed to function: %p\n", array); 
-    for(int i = 0; i < length; i++) array[i] += 1;
 }
