8e387cdd08b3b8cdf0926071ce6eed84e51de450
Matthew Flynn <90197912+mpflynnx@users.noreply.github.com>
2024-03-05 10:56:33 +0000

    70 unions (#74)
    * unions #70
    wip up to section12 chapter 88
    video time 2:43.
    
    * unions #70
    completed section on unions

--- d.c
+++ d.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+
+union sample {
+    int i;
+    char ca[4];
+    float f;
+};
 
 int main()
 {
 
-    int* p = NULL;
-    int a = 5;
-    p = &a; // Assign memory address of a to p
+    union sample u;
+    
+    u.i = 42;
+    printf("%08x %f %s\n", u.i, u.f, u.ca);
+    
+    strcpy(u.ca, "Abc");
+    printf("%08x %f %s\n", u.i, u.f, u.ca);
+    
+    u.f = 1.0/3.0;
+    printf("%08x %f %s\n", u.i, u.f, u.ca);
 
-    return 0;
+	return 0;
+
 }
